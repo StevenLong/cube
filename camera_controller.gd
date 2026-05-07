6c835c46b4a230cf84692ccc9538c786f8580a6f
StevenLong <stevenpatlong@gmail.com>
2026-05-08 00:06:57 +0200

    Isometric camera angle, right stick Y tilt (R/F keyboard)

--- camera_controller.gd
+++ camera_controller.gd
@@ -1,7 +1,12 @@
 extends Camera3D
 
-const OFFSET := Vector3(0.0, 8.0, 8.0)
+const DISTANCE := 12.0
+const H_ANGLE := PI / 4.0   # 45 degrees horizontal - isometric diagonal
+const ELEV_MIN := 0.2        # ~11 degrees - almost side-on
+const ELEV_MAX := 1.3        # ~74 degrees - almost top-down
+const TILT_SPEED := 1.2
 
+var _elevation := 0.6155     # ~35 degrees - true isometric default
 var _target: Node3D
 
 
@@ -9,6 +14,11 @@ func _ready() -> void:
 	_target = get_node("../Player")
 
 
-func _process(_delta: float) -> void:
-	position = _target.position + OFFSET
+func _process(delta: float) -> void:
+	var tilt := Input.get_axis("camera_tilt_down", "camera_tilt_up")
+	_elevation = clampf(_elevation + tilt * TILT_SPEED * delta, ELEV_MIN, ELEV_MAX)
+
+	var h := DISTANCE * cos(_elevation)
+	var v := DISTANCE * sin(_elevation)
+	position = _target.position + Vector3(h * sin(H_ANGLE), v, h * cos(H_ANGLE))
 	look_at(_target.position, Vector3.UP)
