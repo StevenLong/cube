5d42a087aec5b6e57472eafcbba3ac550aee9d90
StevenLong <stevenpatlong@gmail.com>
2026-05-07 23:55:10 +0200

    Fix camera: fixed offset follow, remove orbit
    Switching to fixed camera per design decision. Camera tracks player
    at a constant offset; no orbit control. This also closes task 5 -
    with the camera axis-aligned, D-pad directions match screen directions
    without any additional remapping.

--- camera_controller.gd
+++ camera_controller.gd
@@ -1,10 +1,7 @@
 extends Camera3D
 
-const ORBIT_SPEED := 2.5  # radians per second at full stick
-const DISTANCE := 8.0
-const HEIGHT := 8.0
+const OFFSET := Vector3(0.0, 8.0, 8.0)
 
-var _orbit_angle := 0.0
 var _target: Node3D
 
 
@@ -12,13 +9,6 @@ func _ready() -> void:
 	_target = get_node("../Player")
 
 
-func _process(delta: float) -> void:
-	var input := Input.get_axis("camera_orbit_ccw", "camera_orbit_cw")
-	_orbit_angle += input * ORBIT_SPEED * delta
-
-	position = _target.position + Vector3(
-		sin(_orbit_angle) * DISTANCE,
-		HEIGHT,
-		cos(_orbit_angle) * DISTANCE
-	)
+func _process(_delta: float) -> void:
+	position = _target.position + OFFSET
 	look_at(_target.position, Vector3.UP)
